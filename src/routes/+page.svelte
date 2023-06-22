c02d2368658e742ebb103378a7d19652afc30321
Gunnar Sundberg <gunnarsundberg@pm.me>
2023-06-22 18:27:41 -0400

    fix formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,37 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    //import { ListPlaceholder } from 'flowbite-svelte'
+	import { onMount } from 'svelte';
+	//import { ListPlaceholder } from 'flowbite-svelte'
 	import PlanetTable from './PlanetTable.svelte';
-    
-    // planet data
-    let planets: Planet[] = [];
-    // boolean representing whether all data has been loaded
-    let planetsLoaded = false;
 
-    async function getPlanets() {
-        let next:string;
+	// planet data
+	let planets: Planet[] = [];
+	// boolean representing whether all data has been loaded
+	let planetsLoaded = false;
 
-        const res = await fetch('https://swapi.dev/api/planets/');
-        const data = await res.json();
-        next = data.next;
-        planets = data.results;
-        while (next !== null) {
-            const res = await fetch(next);
-            const data = await res.json();
-            next = data.next;
-            console.log(next);
-            console.log(data.results);
-            planets.push(...data.results);
-            planets = planets;
-        }
-        planetsLoaded = true;
-    }
+	async function getPlanets() {
+		let next: string;
 
-    onMount(async () => {
-        await getPlanets();
-    });
+		const res = await fetch('https://swapi.dev/api/planets/');
+		const data = await res.json();
+		next = data.next;
+		planets = data.results;
+		while (next !== null) {
+			const res = await fetch(next);
+			const data = await res.json();
+			next = data.next;
+			console.log(next);
+			console.log(data.results);
+			planets.push(...data.results);
+			planets = planets;
+		}
+		planetsLoaded = true;
+	}
+
+	onMount(async () => {
+		await getPlanets();
+	});
 </script>
 
-<div class= "flex-auto min-w-0 lg:static lg:max-h-full lg:overflow-visible dark:divide-gray-700">
-    <!--
-    {#if planets.length === 0}
-        <ListPlaceholder divClass="p-4 rounded border border-gray-200 shadow animate-pulse md:p-6 dark:border-gray-700"/>
-    {:else}
-        <PlanetList {planets} {planetsLoaded} />
-    {/if}
-    -->
-    <PlanetTable {planets} {planetsLoaded} />
+<div class="flex-auto min-w-0 lg:static lg:max-h-full lg:overflow-visible dark:divide-gray-700">
+	<PlanetTable {planets} {planetsLoaded} />
 </div>
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,17 +1,12 @@
 <script>
-    import { Footer, FooterCopyright, FooterBrand } from 'flowbite-svelte'
-    import logo from '$lib/assets/deathstar-logo.png'
+	import { Footer, FooterCopyright, FooterBrand } from 'flowbite-svelte';
+	import logo from '$lib/assets/deathstar-logo.png';
 </script>
-  
-  <Footer footerType="logo">
-    <div class="sm:flex sm:items-center sm:justify-between">
-      <FooterBrand
-        href="/"
-        src={logo}
-        alt="Rebel Logo"
-        name="Planet Explorer"
-      />
-    </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-    <FooterCopyright href="/" by="Gunnar Sundberg" />
-  </Footer>
+
+<Footer footerType="logo">
+	<div class="sm:flex sm:items-center sm:justify-between">
+		<FooterBrand href="/" src={logo} alt="Rebel Logo" name="Planet Explorer" />
+	</div>
+	<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+	<FooterCopyright href="/" by="Gunnar Sundberg" />
+</Footer>
